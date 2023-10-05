ee158f4903cef4f1e7e9e2b8370b47d9f13d1d16
beny1122war <tyavbarnabas2@gmail.com>
2023-10-05 14:04:26 +0100

    refactored the security config, removed the deprecated method

--- Product/src/main/java/com/codemarathon/product/serviceImpl/ProductServiceImpl.java
+++ Product/src/main/java/com/codemarathon/product/serviceImpl/ProductServiceImpl.java
@@ -15,7 +15,6 @@ import com.codemarathon.product.utils.ProductUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
